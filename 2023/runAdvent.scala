64b879455c60fdd9d01886534a42ac47f09ae217
SeabassTheFish03 <sneumannthesecond@gmail.com>
2023-12-06 00:25:28 -0500

    Day 2 Part 1, finally

--- 2023/runAdvent.scala
+++ 2023/runAdvent.scala
@@ -1,5 +1,7 @@
 import scala.io.Source
+import scala.util.matching.Regex
 import scala.util.matching.Regex._
+import scala.util.chaining._
 
 def printPass[A](obj: A): A =
 	try
@@ -18,8 +20,7 @@ def printPass[A](obj: A): A =
 				).sum
 			)
 			println("Part 2:\n"+
-				/* So close, but need to replace */
-				Source.fromFile("./Puzzle Inputs/day1.txt").getLines.toList.map(line =>
+				Source.fromFile("./Puzzle Inputs/day1.txt").getLines.map(line =>
 					((patterns: Array[String]) =>
 						Array(
 							patterns.mkString("(","|",")").r.unanchored.findAllMatchIn(line).next.group(1),
@@ -38,5 +39,37 @@ def printPass[A](obj: A): A =
 							).sum
 					).reduceLeft(10*_ + _)
 				).sum
+			)
+		case 2 =>
+			def part1: Int =
+				Source.fromFile("./Puzzle Inputs/day2.txt").getLines.map(line =>
+					Map("red" -> 12, "green" -> 13, "blue" -> 14).pipe(maxMap => 
+						"Game ([0-9]+): (.*)".r.findFirstMatchIn(line).map(matches =>
+							matches.group(2).split("; ").map(mat =>
+								"([0-9]+) (red|blue|green)".r.findAllMatchIn(mat).foldRight(true)((item, accum) =>
+									(maxMap(item.group(2)) >= item.group(1).toInt) && accum
+								)
+							).foldRight(matches.group(1).toInt)((a, b) => if a then b else 0)
+						).getOrElse(0)
+					)
+				).sum
+
+			println("Day 2:")
+			println("Part 1:\n"+
+				/*((gameRegex: Regex) =>
+					Source.fromFile("./Puzzle Inputs/day2.txt").getLines.toList.slice(0, 5).map(line =>
+						line match
+							case gameRegex(game_num, rest, _*) =>
+								(extractorRegex: Regex =>
+									rest.split(";").map(singleGame =>
+										singleGame match
+											/* %d blue/green/red, */
+									)
+									rest match
+										case restRegex =>
+								).apply("([0-9]+) (blue|green|red),?".r)
+					)
+				).apply("Game ([0-9]+): ([0-9]* .*;?)*".r)*/
+				part1.toString
 			)
 		case _ => println("Invalid day")
